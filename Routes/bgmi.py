1f9ce4d45373eb1c9ac695d151030549d2070d0d
sgma-devops <sgma.developer@gmail.com>
2024-07-27 10:43:42 +0530

    changes in get team ranking

--- Routes/bgmi.py
+++ Routes/bgmi.py
@@ -533,6 +533,7 @@ def getTeams(match_id: uuid.UUID, db: Session = Depends(get_db)):
                 "teamName": team_info["teamName"],
                 "logo": team_info["logo"],
                 "kill": team_info["kill"],
+                "rank": team_info["rank"],
                 "players": team_info["alive"],
                 "is_eliminated": False if team_info["alive"] > 0 else True,
                 "alive": sorted(team_info["players"], key=lambda x: x["is_dead"]),
